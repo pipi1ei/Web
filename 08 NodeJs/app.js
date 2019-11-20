614c173721afb03d5a22d2ea83dacc2417697769
pipilei <805396880@qq.com>
2019-11-20 21:01:46 +0800

    add: Vue.js

--- 08 NodeJs/app.js
+++ 08 NodeJs/app.js
@@ -3,25 +3,54 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 
-http.createServer(function (req, res) {
-    var url = req.url;
-    console.log(url);
-    var filename = path.join(__dirname, '/public', url);
-    console.log(filename);
-    if(filename.indexOf('ico') > -1) {
-        console.log('ico: ', filename)
-        res.end();
-    }else{
-        fs.readFile(filename, function (err, data) {
-            if (err) {
-                throw err;
-            } else {
-                res.setHeader('content-type', mime.getType(filename));
-                res.end(data);
-            }
-        })
+// http.createServer(function (req, res) {
+//     var url = req.url;
+//     console.log(url);
+//     var filename = path.join(__dirname, '/public', url);
+//     console.log(filename);
+//     if(filename.indexOf('ico') > -1) {
+//         console.log('ico: ', filename)
+//         res.end();
+//     }else{
+//         fs.readFile(filename, function (err, data) {
+//             if (err) {
+//                 throw err;
+//             } else {
+//                 res.setHeader('content-type', mime.getType(filename));
+//                 res.end(data);
+//             }
+//         })
+//     }
+
+// }).listen(9090, function () {
+//     console.log('http://localhost:9090')
+// })
+
+var util = require('util');
+function Base() {
+    this.name = 'base';
+    this.base = 1991;
+    this.sayHello = function () {
+        console.log('hello ' + this.name);
     }
-    
-}).listen(9090, function () {
-    console.log('http://localhost:9090')
-})
+};
+Base.prototype.showName = function () {
+    console.log(this.name);
+}
+
+function Sub() {
+    this.name = 'sub';
+}
+
+util.inherits(Sub, Base);
+var objBase = new Base();
+objBase.showName();
+objBase.sayHello();
+console.log(objBase);
+console.log(util.inspect(objBase));
+
+var objSub = new Sub();
+objSub.showName();
+// objSub.sayHello();
+console.log(objSub);
+console.log(util.inspect(objSub));
--- vue/readme.md
+++ vue/readme.md
@@ -1,1 +1,11 @@
-### vue 知识学习
+# vue 知识学习
+
+### 创建Vue
+<div id='app'>{{ msg }}</div>
+
+let app = new Vue({
+    el: '#app', // 该属性决定了这个Vue对象挂载到哪一个元素上
+    data: {
+        msg: 'hello world!'   
+    }
+})
