3c09384b7bec2e2cbcc428f9929521c649bfc480
leichen28 <leichen28@iflytek.com>
2019-12-13 17:45:54 +0800

    修改 vue.config.js 配置文件

--- meadia_system/vue.config.js
+++ meadia_system/vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  assetsDir: 'static',
   configureWebpack: {
     resolve: {
       alias: {
