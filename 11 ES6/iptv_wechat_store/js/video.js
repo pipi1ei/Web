d4b087a1ac557c42c50866f9078bcf90fee053d5
leichen28 <leichen28@iflytek.com>
2020-04-26 10:10:47 +0800

    提交代码

--- 11 ES6/iptv_wechat_store/js/video.js
+++
@@ -1,8 +0,0 @@
-onload = function () {
-    initBuryPoint(false,"","");
-    onEvent("FT03001",{"extend":"了解更多页面打开"},"1","","");
-};
-
-function onVideoPlay() {
-    onEvent("FT03002",{"extend":"播放视频"},"1","","");
-}
